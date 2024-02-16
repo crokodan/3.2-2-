8c15669d0dee6f233fc8e08d37091d3105b667fa
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 18:51:07 +0400

    Add files via upload

---
+++ 3.2 Задание 2)/3.2 Задание 2)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2_Задание_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, z, k;
+            Console.Clear();
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+            z = 1;
+            k = 0;
+            while (z < n - k)
+            {
+                z = z + k;
+                k = z - k;
+                Console.Write(z + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
